b4376b71df588ade7e4f4fc78c3c7e056898e6cc
Proofee <proofee@mail.ru>
2022-12-19 15:09:13 +0300

    Правки

--- src/main/java/recipe/recipes_book/recipe_book/controller/RecipeController.java
+++ src/main/java/recipe/recipes_book/recipe_book/controller/RecipeController.java
@@ -1,48 +1,29 @@
 package recipe.recipes_book.recipe_book.controller;
 
 import org.springframework.web.bind.annotation.*;
-import recipe.recipes_book.recipe_book.model.Ingredient;
 import recipe.recipes_book.recipe_book.model.Recipe;
-import recipe.recipes_book.recipe_book.service.impl.IngredientServiceImpl;
 import recipe.recipes_book.recipe_book.service.impl.RecipeServiceImpl;
 
 import java.util.Collections;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/recipe")
+@RequestMapping("/recipes")
 public class RecipeController {
 
     private final RecipeServiceImpl recipeService;
 
-    private final IngredientServiceImpl ingredientService;
-
-    public RecipeController(RecipeServiceImpl recipeService, IngredientServiceImpl ingredientService) {
+    public RecipeController(RecipeServiceImpl recipeService) {
         this.recipeService = recipeService;
-        this.ingredientService = ingredientService;
     }
 
-    @GetMapping()
-    public String project(){
-        return "Приложение запущено";
-    }
-
-    @PostMapping("/createRecipe/{id}")
-    public Recipe createRecipe(@PathVariable("id")String id, @RequestBody Recipe recipe) {
+    @PostMapping
+    public Recipe createRecipe(String id, @RequestBody Recipe recipe) {
         return this.recipeService.addRecipe(id,recipe);
     }
-    @PostMapping("/createIngredient/{id}")
-    public Ingredient createIngredient(@PathVariable("id")String id, @RequestBody Ingredient ingredient) {
-        return this.ingredientService.addIngredient(id,ingredient);
-    }
 
-    @GetMapping("/getRecipes/{id}")
-    public Set<String> getAllRecipes(@PathVariable("id") String id ) {
+    @GetMapping("/{id}")
+    public Set<String> getRecipeById(@PathVariable("id") String id ) {
         return Collections.singleton(this.recipeService.getRecipe(id).toString());
     }
-    @GetMapping("/getIngredients/{id}")
-    public Set<String> getAllIngredients(@PathVariable("id") String id ) {
-        return Collections.singleton(this.ingredientService.getIngredient(id).toString());
-    }
-
 }
